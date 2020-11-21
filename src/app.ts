d0c7e5e97d27d8183abf941fd422aed4fdcce3c3
Jerry Macedo Castro <leom551@hotmail.com>
2020-11-21 17:17:15 -0300

    Post nout found error handler created

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import * as bodyParser from 'body-parser';
 import express from 'express';
 import mongoose from 'mongoose';
 import Controller from './interfaces/controller.interface';
+import errorMiddleware from './middleware/error.middleware';
 
 class App {
   public app: express.Application;
@@ -22,6 +23,10 @@ class App {
 
   private initializeMiddlewares() {
     this.app.use(bodyParser.json());
+  }
+
+  private initializeErrorHandling() {
+    this.app.use(errorMiddleware);
   }
 
   private initializeControllers(controllers: Controller[]) {
